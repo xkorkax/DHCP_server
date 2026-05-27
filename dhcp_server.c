faf95588a06ee40d42a68c6f5a21a3ca8d36a6ce
xkorkax <kornelia.balicka@icloud.com>
2026-05-27 19:28:26 +0200

    create template

---
+++ dhcp_server.c
@@ -0,0 +1,10 @@
+// gcc dhcp_server.c -o dhcp_server
+// clang dhcp_server.c -o dhcp_server
+// ./dhcp_server
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, DHCP Server!\n");
+    return 0;
+}
